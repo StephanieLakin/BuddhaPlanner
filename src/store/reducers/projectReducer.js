05a1f342fbef3b66e770e451f92aa33b7c4830b8
Stephanie Lakin <stephanielakin13@outlook.com>
2019-11-23 17:35:56 -0500

    setting up async &  thunk

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -7,6 +7,12 @@ const initState = {
 }
 
 const projectReducer = (state = initState, action) => {
+    // eslint-disable-next-line default-case
+    switch  (action.type) {
+        case 'CREATE_PROJECT':
+        console.log('created project', action.project)
+    }
+
     return state
 }
 
