791d5c720a17f480a9851028a25c89ccee0e9e10
Stephanie Lakin <stephanielakin13@outlook.com>
2019-12-19 00:54:28 -0500

    rendering correct links on sign in  or out

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,22 +4,23 @@ import SignInLinks from '../../components/layout/SignInLinks'
 import SignOutLinks from '../../components/layout/SignOutLinks'
 import { connect } from 'react-redux'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const {auth} = props;
+    //console.log(auth)
+    const links = auth.uid ? <SignInLinks /> :  <SignOutLinks />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo left-align left">Buddha Planner</Link>
-                <SignInLinks />
-                <SignOutLinks />
+                {links}
             </div>
         </nav>
     )
 }
-
 const mapStateToProps = (state) => {
-    console.log(state);
+  //  console.log(state);
     return {
-
+        auth: state.firebase.auth
     }
 }
 export default connect(mapStateToProps)(Navbar)
--- src/components/layout/SignInLinks.js
+++ src/components/layout/SignInLinks.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {signOut} from '../../store/actions/authActions'
 
-export const SignInLinks = () => {
+export const SignInLinks = (props) => {
    return (
         <ul className="right">
                 <li>
                 <NavLink to='/createproject'>New Project</NavLink>
                 </li>
                 <li> 
-                   <NavLink to='/'>Log Out</NavLink>
+                   <a onClick={props.signOut}>Log Out</a>
                 </li>           
                 <li>
                     <NavLink to='/' className="btn btn-floating pink lighten-1">SJL</NavLink>
@@ -16,4 +18,10 @@ export const SignInLinks = () => {
         </ul>
    )
 }
-export default SignInLinks
+
+const mapDispatchToProps = (dispatch) => {
+return {
+   signOut: () => dispatch(signOut())
+}
+}
+export default connect(null, mapDispatchToProps)(SignInLinks)
